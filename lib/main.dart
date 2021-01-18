915e75d65f12b0f4db63bb83abed87fc129d3988
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-01-18 20:47:22 -0200

    botao,appbar,constante de perguntas , visual pro app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  final perguntas = [
+    'Qual sua cor Favorita?',
+    'Qual seu animal favorito?',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +15,15 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola vitor'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
